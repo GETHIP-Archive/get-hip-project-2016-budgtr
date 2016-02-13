152cb25809eb93ee849d07ea2f0753d013a01475
kgerot442 <kegerot@cox.net>
2016-02-13 11:59:40 -0600

    Added UI Files

---
+++ UI/contact/contact.php
@@ -0,0 +1,19 @@
+<?php
+/*
+Credits: Bit Repository
+URL: http://www.bitrepository.com/
+*/
+
+include 'config.php';
+
+error_reporting (E_ALL ^ E_NOTICE);
+
+$post = (!empty($_POST)) ? true : false;
+
+if($post)
+{
+
+$name = stripslashes($_POST['name']);
+$email = trim($_POST['email']);
+$subject = stripslashes($_POST['subject']);
+$message = stripslashes($_POST['message']);
