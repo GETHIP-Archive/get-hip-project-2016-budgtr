0517326a6f7c64a415e9413b6ec94db1fd6e25b9
Harley Waldstein <hwaldstein1997@gmail.com>
2016-02-27 11:50:33 -0600

    Allow login form to click through to dashboard

--- src/main/java/io/waldstein/gethip/budgtr/UserResource.java
+++ src/main/java/io/waldstein/gethip/budgtr/UserResource.java
@@ -2,19 +2,27 @@ package io.waldstein.gethip.budgtr;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.avaje.ebean.Ebean;
 
-import io.waldstein.gethip.budgtr.model.User;	
+import io.waldstein.gethip.budgtr.model.User;
 
 @Path("/user")
 public class UserResource {
+	
+	private static Logger logger = LoggerFactory.getLogger(UserResource.class);
 
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
@@ -24,14 +32,14 @@ public class UserResource {
 
 	@POST
 	@Produces(MediaType.APPLICATION_JSON)
-	public User postUser(User u){	
+	public User postUser(User u) {
 		Ebean.save(u);
 		return u;
 	};
 
 	@PUT
 	@Produces(MediaType.APPLICATION_JSON)
-	public User putUser(@QueryParam("id") long id, User u){
+	public User putUser(@QueryParam("id") long id, User u) {
 		User PUT = User.find.where().eq("id", id).findUnique();
 		PUT.firstName(u.firstName);
 		PUT.lastName(u.lastName);
@@ -43,9 +51,19 @@ public class UserResource {
 
 	@DELETE
 	@Produces(MediaType.APPLICATION_JSON)
-	public User deleteUser(@QueryParam("id") long id){
+	public User deleteUser(@QueryParam("id") long id) {
 		User u = User.find.where().eq("id", id).findUnique();
 		Ebean.delete(u);
 		return u;
 	};
+
+	
+	@POST
+	@Path("auth")
+	public void auth(@FormParam("email") String email, @FormParam("pwd") String password) {
+		// Do something
+		logger.info("Email: " + email);
+		logger.info("Password: " + password);
+	}
+	
 }
